26ebf10250f9e04742b0a781b70ea5d7f7cf475a
Drekken <drekken.pownz@protonmail.com>
2025-02-12 22:19:22 -0500

    reduce computer for debug

--- run.py
+++ run.py
@@ -73,7 +73,7 @@ def main():
             maps.get(random.choice(map_list)),
             [
                 bot1,
-                Computer(Race.Zerg, Difficulty.Hard, ai_build=AIBuild.Rush),
+                Computer(Race.Zerg, Difficulty.VeryEasy, ai_build=AIBuild.Macro),
             ],
             realtime=False,
         )
