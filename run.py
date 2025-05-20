578c477b011390f1f893c151eeb7033f8cf91f10
Drekken <drekken.pownz@protonmail.com>
2025-05-19 20:32:00 -0400

    set race to protoss

--- run.py
+++ run.py
@@ -73,7 +73,7 @@ def main():
             maps.get(random.choice(map_list)),
             [
                 bot1,
-                Computer(Race.Terran, Difficulty.Harder, ai_build=AIBuild.Macro),
+                Computer(Race.Protoss, Difficulty.VeryEasy, ai_build=AIBuild.Macro),
             ],
             realtime=False,
         )
