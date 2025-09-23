7bff07f4a0c6021443aa01baa20e2306a197ee13
Drekken <drekken.pownz@protonmail.com>
2025-09-22 20:16:10 -0400

    new test scenario

--- run.py
+++ run.py
@@ -73,11 +73,11 @@ def main():
         # Local game
         
         map_list: List[str] = [
-            #"TorchesAIE_v4",
-            #"PylonAIE_v4",
-            #"PersephoneAIE_v4",
-            #"IncorporealAIE_v4",
-            #"LeyLinesAIE_v3",
+            "TorchesAIE_v4",
+            "PylonAIE_v4",
+            "PersephoneAIE_v4",
+            "IncorporealAIE_v4",
+            "LeyLinesAIE_v3",
             "UltraloveAIE_v2"
 
         ]
@@ -87,7 +87,7 @@ def main():
             maps.get(random.choice(map_list)),
             [
                 bot1,
-                Computer(Race.Zerg, Difficulty.VeryHard, ai_build=AIBuild.Rush),
+                Computer(Race.Protoss, Difficulty.VeryHard, ai_build=AIBuild.Macro),
             ],
             realtime=False,
         )
