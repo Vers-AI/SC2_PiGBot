1c6f463f9864f1f0393b397b6bf4fff414666675
Drekken <drekken.pownz@protonmail.me>
2025-10-16 20:14:10 -0400

    chore: changed to rush

--- run.py
+++ run.py
@@ -88,7 +88,7 @@ def main():
             maps.get(random.choice(map_list)),
             [
                 bot1,
-                Computer(Race.Zerg, Difficulty.VeryHard, ai_build=AIBuild.Macro),
+                Computer(Race.Zerg, Difficulty.VeryHard, ai_build=AIBuild.Rush),
             ],
             realtime=False,
         )
